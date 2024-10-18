9775544ac31bf1aca0ef39d15f086cb56636040f
Varsha <Varsha@DESKTOP-2NAMJSI>
2024-10-18 13:01:46 +0530

    jenkins second commit

---
+++ For_Jenkins/src/test/java/contactModule/CreateContactWithOrg.java
@@ -0,0 +1,14 @@
+package contactModule;
+
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrg {
+	
+	@Test
+	public void createContactWithOrganization() {
+		
+		
+		System.out.println("Created contact with org");
+	}
+
+}
